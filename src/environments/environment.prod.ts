f52474f36504ca81960ba23bd6583a34a94d3c02
Jair Mancilla Molina <jaiman@MacBook-Pro-de-Jair.local>
2026-09-12 15:26:33 -0600

    definitive url to microservices

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  authApiUrl: 'http://auth-mhm0d92ttfsc887lizhbqobe.5.78.222.52.sslip.io/api/Auth',
-  profileApiUrl: 'http://profile-odc45xuaftxe4vc8ln3ut4xi.5.78.222.52.sslip.io/api/Profile',
+  authApiUrl: 'https://auth.lavanderiaatucasa.com/api/Auth',
+  profileApiUrl: 'https://profile.lavanderiaatucasa.com/api/Profile',
   catalogsApiUrl:
-    'http://catalogs-rzm1yxr4ri92lwl3ebswrd8u.5.78.222.52.sslip.io/api/Catalogs',
-  ordersApiUrl: 'http://orders-qcy3lpqvgvjumcg245cppo4i.5.78.222.52.sslip.io/api/Orders',
+    'https://catalogs.lavanderiaatucasa.com/api/Catalogs',
+  ordersApiUrl: 'https://orders.lavanderiaatucasa.com/api/Orders',
   notificationsApiUrl:
-    'http://notifications-h2q2d1rqdor215xiyz0c5359.5.78.222.52.sslip.io/api/Notifications'
+    'https://notifications.lavanderiaatucasa.com/api/Notifications'
 };
